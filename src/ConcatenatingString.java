f6ea7a9d79ba1e1bf266f50c9332931e712b8155
Alok Jha <axjh03@gmail.com>
2023-08-31 08:44:56 -0500

    few more codes

--- src/ConcatenatingString.java
+++ src/ConcatenatingString.java
@@ -1,6 +1,6 @@
 public class ConcatenatingString
 {
-    public static void main(String[] args)
+    public static void main (String[] args)
     {
         String studentFirstName = "Aalok";
         String studentLastName = "Jha";
